99ecc11ae4a987713c7e0a939223e67ea7fd5615
WithuL <1509586319@qq.com>
2020-07-15 15:25:21 +0800

    上传文件

---
+++ java16/src/main/java/java16_0713/ServletDemo10.java
@@ -0,0 +1,23 @@
+package java16_0713;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+
+@MultipartConfig
+public class ServletDemo10 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String basePath = "F:/Bit/tmp/";
+        Part image =  req.getPart("image");
+        String path = basePath + image.getSubmittedFileName();
+        image.write(path);
+
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().write("文件上传成功哦o(><；)oo");
+    }
+}
