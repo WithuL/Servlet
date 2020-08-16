08877b75f0ea4b2cf7a7e5a737c8d504fe411bdc
WithuL <1509586319@qq.com>
2020-08-16 17:58:09 +0800

    Spring Test

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import animal.Duck;
 import animal.Duck2;
+import animal.Duck3;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,8 +11,14 @@ public class Main {
         String bit1 = (String) context.getBean("bit");
         Duck d1 = (Duck) context.getBean("d1");
         Duck2 d2 = (Duck2) context.getBean("d2");
-        System.out.println(bit1);
-        System.out.println(d1);
-        System.out.println(d2);
+
+        Duck3 k1 = (Duck3) context.getBean("k3");
+        while(k1 != null) {
+            System.out.println(k1);
+            k1 = k1.getNext();
+        }
+//        System.out.println(bit1);
+//        System.out.println(d1);
+//        System.out.println(d2);
     }
 }
